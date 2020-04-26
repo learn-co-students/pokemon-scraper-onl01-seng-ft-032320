439c927c8187bb9df7da2c90f7fa35b42c45e6ed
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-04-26 02:09:43 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  
+  def initialize(id:nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS db(id INTEGER PRIMARY KEY, name TEXT, type TEXT);"
+    db.execute(sql)
+  end
+  
+  def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?);", name, type)
+  end
+  
+  def self.find(id, db)
+    results = db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
+    id = results[0][0]
+    name = results[0][1]
+    type = results[0][2]
+    db = results[0][3]
+    
+    pokemon = self.new(id: id, name: name, type: type, db: db)
+  end
+  
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Scraper
   attr_accessor :file, :parsed_file, :all_pokemon, :db
 
@@ -6,6 +8,7 @@ class Scraper
     self.file = File.open("pokemon_index.html")
     self.parsed_file = Nokogiri::HTML.parse(file)
     self.all_pokemon = catch_em_all
+    binding.pry
   end
 
   def catch_em_all
